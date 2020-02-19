8818c3e08450af11aa6f05c6718c0a0bb3e49736
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-02-19 11:07:09 +0100

    simple static textures

--- static/js/material.js
+++ static/js/material.js
@@ -11,8 +11,27 @@ export function getMaterial(nodeName){
       return;
     }
 
-    var color = new THREE.Color( colorObj.r/256, colorObj.g/256, colorObj.b/256 );
-    material = new THREE.MeshBasicMaterial( { color: color } );
+		if (nodeName == "default:stone") {
+			let texture = new THREE.TextureLoader().load("textures/default_stone.png");
+			material = new THREE.MeshBasicMaterial( { map: texture } );
+
+		}	else if (nodeName == "default:dirt") {
+			let texture = new THREE.TextureLoader().load("textures/default_dirt.png");
+			material = new THREE.MeshBasicMaterial( { map: texture } );
+
+		}	else if (nodeName == "default:dirt_with_grass") {
+			let texture = new THREE.TextureLoader().load("textures/default_grass.png");
+			material = new THREE.MeshBasicMaterial( { map: texture } );
+
+		}	else if (nodeName == "default:sand") {
+			let texture = new THREE.TextureLoader().load("textures/default_sand.png");
+			material = new THREE.MeshBasicMaterial( { map: texture } );
+
+		} else {
+			let color = new THREE.Color( colorObj.r/256, colorObj.g/256, colorObj.b/256 );
+			material = new THREE.MeshBasicMaterial( { color: color } );
+
+		}
 
 		if (nodeName == "default:water_source"){
 			material.transparent = true;
